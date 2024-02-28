04401eb4c380d8f434cd09e0228b7a5621ecee7e
iamhextor <72164644+iamhextor@users.noreply.github.com>
2024-02-28 19:07:52 +0530

    Add files via upload

---
+++ js/commands.js
@@ -0,0 +1,87 @@
+// These are the variable for the direct links
+var twitter = "https://www.twitter.com/iamdevansharya";
+var linkedin = "https://www.linkedin.com/in/iamdevansharya";
+var instagram = "https://www.instagram.com/iamdevansharya";
+var github = "https://github.com/iamhextor";
+var email = 'mailto:iamdevansharya@gmail.com';
+var medium = 'https://medium.com/@iamdevansharya';
+
+// This content is about the "WHOIS"
+whois = [
+  "<br>",
+  "- Devansh Arya is a Security Consultant",
+  "<br>",
+  "- Devansh specializes in conducting Red Team Assessments & Penetration Testing for web applications, network, and Active Directory. In the Application Security, he worked in Vulnerability Scanning and Engineering, DAST (Qualys, Burpsuite, Invicti, Insight AppSec), SAST(Veracode) and Vulnerability Threat Intelligence.",
+  "<br>",
+  "- Devansh is an active member of the ILUGD, Hack the Box and FOSS community and participates in solving new security challenges",
+  "<br>"
+];
+
+social = [
+  "<br>",
+  'twitter        <a href="' + twitter + '" target="_blank">twitter/iamdevansharya' + '</a>',
+  'linkedin       <a href="' + linkedin + '" target="_blank">linkedin/iamdevansharya' + "</a>",
+  'instagram      <a href="' + instagram + '" target="_blank">instagram/iamdevansharya' + '</a>',
+  'github         <a href="' + github + '" target="_blank">github/iamhextor' + "</a>",
+  "<br>"
+];
+
+projects = [
+  "<br>",
+  "Still curating... most projects are offline, on GitHub, or confidential.",
+  "<br>"
+];
+
+interview = [
+//  "<br>",
+//  '1. Interview Preparation <a href="' + interviewprep + '" target="_blank">Interview Question' + "</a>",
+//  '2. Threat Modeling Preparation <a href="' + threatmodeling + '" target="_blank">Threat Modeling' + "</a>",
+  "<br>"
+];
+
+blogs = [
+  "<br",
+//  '1. Wireguard : A new tunneling protocol <a href="' + wiregaurd + '" target="_blank">medium' + "</a>", 
+//  '2. Tales of XSS: Navigating Web Vulnerabilities <a href="' + xss + '" target="_blank">medium' + "</a>", 
+//  '3. A Long Story of XXE Vulnerability!! <a href="' + xxe + '" target="_blank">medium' + "</a>", 
+//  '4. iOS Application Vulnerability Assessment and Penetration Testing. <a href="' + ios + '" target="_blank">medium' + "</a>", 
+//  '5. Android Vulnerability Assessment and Penetration Testing Part 1 <a href="' + androidp1 + '" target="_blank">medium' + "</a>", 
+//  '6. Android Vulnerability Assessment and Penetration Testing Part 2 <a href="' + androidp2 + '" target="_blank">medium' + "</a>", 
+//  '7. Bypass Blocked IP with the X-Forwarded For Header <a href="' + ipbypass + '" target="_blank">medium' + "</a>", 
+//  '8. XSS Automation using Waybackurl And gf (Grep-Finding) <a href="' + xssautomation + '" target="_blank">medium' + "</a>", 
+//  '9. Why localhost and BurpSuite are not best friends. <a href="' + burplocalhost + '" target="_blank">medium' + "</a>"
+];
+
+secret = [
+  "<br>",
+  '<span class="command">sudo</span>           Only use if you\'re admin',
+  "<br>"
+];
+
+help = [
+  "<br>",
+  '<span class="command">whois</span>          About Me',
+  '<span class="command">whoami</span>         Who are you ?',
+  '<span class="command">video</span>          My Youtube Channel',
+  '<span class="command">social</span>         My Social platforms',
+  '<span class="command">projects</span>       My Open Source Projects',
+//  '<span class="command">resume</span>         My Resume',
+//  '<span class="command">wiki</span>           Nightingale Wiki',
+//  '<span class="command">conference</span>     Conference list',
+//  '<span class="command">blog</span>           My Medium Blog',
+  '<span class="command">history</span>        You know what it is:p',
+  '<span class="command">email</span>          My Email address',
+  '<span class="command">interview</span>      Interview',
+  '<span class="command">clear</span>          Clear terminal',
+  "<br>",
+];
+
+banner = [
+  "<br>",
+  "+-+-+-+-+-+-+-+-+-+-+-+-+",
+  "|D|E|V|A|N|S|H| |A|R|Y|A|",
+  "+-+-+-+-+ +-+-+-+-+-+-+-+",
+  "<span class=\"color2\">Welcome to my interactive web terminal</span>",
+  "<span class=\"color2\">For a list of available commands, type</span> <span class=\"command\">'help'</span><span class=\"color2\">.</span>",  
+  "<span class=\"color2\">Note: Don't think about</span> <span class=\"command\">'sudo'</span><span class=\"color2\">.</span>"
+];
